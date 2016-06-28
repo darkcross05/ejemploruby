717c0ec59a404bcdd7b9f81e8b9ce115e2124b39
Juan <juanpa96@live.com.mx>
2016-06-28 17:32:50 -0500

    ruby

---
+++ prueba.rb
@@ -0,0 +1,25 @@
+#3.times{puts "hello world"}
+
+#if a+2
+#	b+3
+#end
+
+#esto es un comentario
+#p hace referencia a la print
+
+#consola de ruby irb
+
+# a las variables no deben decirle el tipo de dato, no hay punto y coma para cerrar
+#a las variables todo en minuscula y a las constantes deben ser en mayuscula o por lo menos la primiera
+#las clases y funciones la primera letra de cada palabra en Mayuscula
+
+#puts 3
+
+#p 3
+
+#f= 9; g=8
+
+
+puts "Ingresa un número"#donde se solicita el codigo
+a = gets.chomp.to_i#se captura el dato y lo convierte en entero
+puts a +10#se muestra y se suma ya sea puts o p
